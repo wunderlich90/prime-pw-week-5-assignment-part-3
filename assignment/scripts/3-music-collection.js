023fb8502ed16e5a5610c17100712d15bc6ed029
Jeremy Cady <jcady90@icloud.com>
2021-07-10 23:05:26 -0500

    started showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,27 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection (title, artist, yearPublished){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+    }
+  collection.push(album);
+  return album;
+}
+
+addToCollection ('Purple Rain', 'Prince', '1984');
+addToCollection ('The Questions', 'Kurt Elling', '2018');
+addToCollection ('Flor', 'Gretchen Parlato', '2021');
+addToCollection ('Bach Cello Suites', 'Pierre Fournier', '2017');
+addToCollection ('Night Dreamer', 'Wayne Shorter', '1964');
+addToCollection ('Stop/Shutup/Shout It Out', 'Bernhoft', '2016');
+addToCollection ('Djesse Vol. 3', 'Jacob Collier', '2020');
+
+//console.log(collection);
+
+function showCollection (collectionArray[]) {
+  
+}
