f4a32240e4331436ed19abcfb7b9919f3b79978b
Jeremy Cady <jcady90@icloud.com>
2021-07-11 09:23:36 -0500

    started showColletion

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,9 @@
 console.log('***** Music Collection *****')
 
+// Declare empty array
 let collection = [];
 
+// Write a function to create album objects and add then to the array
 function addToCollection (title, artist, yearPublished){
   let album = {
     title: title,
@@ -12,16 +14,27 @@ function addToCollection (title, artist, yearPublished){
   return album;
 }
 
-addToCollection ('Purple Rain', 'Prince', '1984');
-addToCollection ('The Questions', 'Kurt Elling', '2018');
-addToCollection ('Flor', 'Gretchen Parlato', '2021');
-addToCollection ('Bach Cello Suites', 'Pierre Fournier', '2017');
-addToCollection ('Night Dreamer', 'Wayne Shorter', '1964');
-addToCollection ('Stop/Shutup/Shout It Out', 'Bernhoft', '2016');
-addToCollection ('Djesse Vol. 3', 'Jacob Collier', '2020');
 
-//console.log(collection);
+// Adding albums using the addToCollection function
+console.log('Album added: ', addToCollection ('Purple Rain', 'Prince', '1984'))
+console.log('Album added: ', addToCollection ('The Questions', 'Kurt Elling', '2018'));
+console.log('Album added: ', addToCollection ('Flor', 'Gretchen Parlato', '2021'));
+console.log('Album added: ', addToCollection ('Bach Cello Suites', 'Pierre Fournier', '2017'));
+console.log('Album added: ', addToCollection ('Night Dreamer', 'Wayne Shorter', '1964'));
+console.log('Album added: ', addToCollection ('Stop/Shutup/Shout It Out', 'Bernhoft', '2016'));
+console.log('Album added: ', addToCollection ('Djesse Vol. 3', 'Jacob Collier', '2020'));
 
-function showCollection (collectionArray[]) {
-  
+
+function showCollection (collectionArray) {
+  console.log(collectionArray.length);
+  for(let i=0; i<collectionArray.length; i++) {
+    console.log(`${title} by ${artist}
+      published in ${yearPublished}`);
+  }
+}
+
+showCollection (collection);
+
+function findByArtist (artist){
+  let resultArray = [];
 }
